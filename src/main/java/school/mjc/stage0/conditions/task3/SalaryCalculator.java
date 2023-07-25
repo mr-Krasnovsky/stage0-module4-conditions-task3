321d5fe77ce86bac3de1aaa1b3c85a799d2bbc55
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-07-25 14:43:23 +0300

    Complited

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+//        Implement the program that will consume salary from method argument as input of an employee and
+//        calculate its salary after taxes according to the following:
+//        Basic Salary <= 10000: taxes - 15%.
+//        10000 < Basic Salary <= 20000: taxes - 18%
+//                Basic Salary > 20000 : taxes - 20%
+//                Basic salary < 0 -> "wrong input!"
+
+        if (salary <= 10000 && salary >= 0) {
+            System.out.println(((double) salary) * 0.85);
+        } else if (salary > 10000 && salary <= 20000) {
+            System.out.println(((double) salary) * 0.82);
+        } else if (salary > 20000) {
+            System.out.println(((double) salary) * 0.8);
+        } else {
+            System.out.println("wrong input!");
+        }
     }
 }
